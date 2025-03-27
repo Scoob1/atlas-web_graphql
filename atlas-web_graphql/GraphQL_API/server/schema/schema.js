375d8dbe607227a8dc2da4d4b42c80ce6d928ee7
nooboftime <tamara.walling@atlasschool.com>
2025-03-26 20:20:32 -0500

    added package json

--- atlas-web_graphql/GraphQL_API/server/schema/schema.js
+++ atlas-web_graphql/GraphQL_API/server/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const TaskType = new GraphQLObjectType({
   name: 'Task',
@@ -11,5 +11,26 @@ const TaskType = new GraphQLObjectType({
   }
 });
 
-module.exports = TaskType;
+// Sample data
+const tasks = [
+  { id: '1', title: 'Task 1', weight: 5, description: 'First Task Description' },
+  { id: '2', title: 'Task 2', weight: 10, description: 'Second Task Description' }
+];
 
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return tasks.find(task => task.id === args.id);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});

f0f0b2ed1d55f6df74ef3c8829f20b3a108781c3
nooboftime <tamara.walling@atlasschool.com>
2025-03-26 19:52:31 -0500

    schema

---
+++ atlas-web_graphql/GraphQL_API/server/schema/schema.js
@@ -0,0 +1,15 @@
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const TaskType = new GraphQLObjectType({
+  name: 'Task',
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    description: { type: GraphQLString }
+  }
+});
+
+module.exports = TaskType;
+
